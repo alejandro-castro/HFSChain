564e7ea76caf55e30c1f7297396e9dc6f018f014
Alejandro Castro <castro.alejandro@pucp.edu.pe>
2019-06-25 11:11:40 -0500

    Modifying a little the comment and adding a parameter to sending_script

--- schainroot/source/schainpy/SendingScripts/sending_script.sh
+++ schainroot/source/schainpy/SendingScripts/sending_script.sh
@@ -3,6 +3,7 @@ echo "INICIANDO SECUENCIA DE CONSOLAS"
 
 echo "EJECUTANDO PROGRAMA DE ENVIO"
 cd /home/igp-114/TestReduccionDatos_Implementado/hfschain/schainroot/source/schainpy/SendingScripts
-#The only parameter of this script is going to be any of the following strings: params, out, rtdi
+#The first parameter is the location
+#The second parameter of this script is going to be any of the following strings: params, out, rtdi
 #That string is going to pass through to the send_SCP.py and set the paths to send the correct type of result
-screen -S "ENVIO_WEB" -d -m ./sendResults_SCP.sh  0  1  2  /home/igp-114/Pictures/   11    $1
+screen -S "ENVIO_WEB" -d -m ./sendResults_SCP.sh  0  1  2  /home/igp-114/Pictures/   $1    $2
